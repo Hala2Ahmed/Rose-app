9699b591eea8ba2a1db61c8180888f30f0a46f5c
Hala2Ahmed <hkishawi81@gmail.com>
2026-01-07 21:09:19 +0200

    feat(Gallery): add Gallery section

---
+++ src/app/(homepage)/_features/gallery/components/gallery-section.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React from "react";
+// import MainTitle from "@/components/shared/shard-title";
+import SubTitle from "@/components/shared/sub-title";
+
+export default function Gallery() {
+  const galleryImages = [
+    { src: "/assets/images/image11.png", height: "38.563rem" },
+    { src: "/assets/images/image12.png", height: "25.3rem" },
+    { src: "/assets/images/image5.png", height: "25.688rem" },
+    { src: "/assets/images/image10.png", height: "38.188rem" },
+    { src: "/assets/images/image10.png", height: "25.688rem" },
+    { src: "/assets/images/image7.png", height: "38.188rem" },
+  ];
+
+  return (
+    <section>
+      {/* Section Header */}
+      <SubTitle className="text-center" title="Gallery" />
+      <header className="text-center mt-2 mb-10">
+        {/* <MainTitle title="Check Out our Wonderful Gallery" />  This code is based on someone else's code, so I will add it later. */}
+      </header>
+
+      {/* Masonry Gallery */}
+      <section className="columns-3 gap-3 space-y-3.5">
+        {galleryImages.map((image, index) => (
+          <div
+            key={index}
+            className="relative w-full break-inside-avoid"
+            style={{ height: image.height }}
+          >
+            <Image
+              src={image.src}
+              alt="Gallery image"
+              fill
+              className="object-cover"
+            />
+          </div>
+        ))}
+      </section>
+    </section>
+  );
+}
--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,11 +1,17 @@
 import About from "./_features/about/components/about-section";
-import Companies from "./_features/Companies/components/companies-section";
+import Companies from "./_features/companies/components/companies-section";
+import Gallery from "./_features/gallery/components/gallery-section";
 
 export default function Home() {
   return (
     <>
+      {/* About Section */}
       <About />
-      
+
+      {/* Gallery Section */}
+      <Gallery />
+
+      {/* Companies Section */}
       <Companies />
     </>
   );
