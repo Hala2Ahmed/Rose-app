617752b2171cb0a3a18be1c7a54b222a70ee74cb
Hala2Ahmed <hkishawi81@gmail.com>
2026-01-07 18:46:11 +0200

    feat(companies): add companies section

--- src/app/(homepage)/page.tsx
+++ src/app/(homepage)/page.tsx
@@ -1,9 +1,12 @@
 import About from "./_features/about/components/about-section";
+import Companies from "./_features/Companies/components/companies-section";
 
 export default function Home() {
   return (
     <>
       <About />
+      
+      <Companies />
     </>
   );
 }
