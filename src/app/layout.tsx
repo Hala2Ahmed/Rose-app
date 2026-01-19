d8fffb6cbf30649cb41f0962793abdca5883a1a2
sarah hesham <sarah.h.nashaat@gmail.com>
2026-01-19 22:38:05 +0200

    Revert merge commit - undoing previous merge
       Revert "(refactor): remove html and body from rootlayout"
    
    This reverts commit acda1203579622dabef3dbfeca86ca3ef883715c.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,37 @@
 import "./globals.css";
+import localFont from "next/font/local";
+import { Sarabun, Tajawal } from "next/font/google";
+import { cn } from "@/lib/utils/tailwind-merge";
+const sarabun = Sarabun({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700", "800"],
+  variable: "--font-sarabun",
+});
 
+const tajawal = Tajawal({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700", "800"],
+  variable: "--font-tajawal",
+});
+
+const edwardianScript = localFont({
+  src: "./fonts/edwardianscriptitc.ttf",
+  variable: "--font-edwardian",
+  display: "swap",
+});
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return children;
+  return (
+    <html>
+      <body
+        className={cn(
+          `${sarabun.variable} ${tajawal.variable} ${edwardianScript.variable} antialiased`,
+        )}>
+        {children}
+      </body>
+    </html>
+  );
 }
