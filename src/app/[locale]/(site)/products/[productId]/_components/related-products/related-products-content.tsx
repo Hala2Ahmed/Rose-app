1c0fb025c0398d5074205e139a26c1455c874862
LaraSamara <larasamara2002@gmail.com>
2026-02-07 23:44:30 +0200

    handles empty related products data

--- src/app/[locale]/(site)/products/[productId]/_components/related-products/related-products-content.tsx
+++ src/app/[locale]/(site)/products/[productId]/_components/related-products/related-products-content.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { getSimilarProductService } from '../../_services/similar-products/similar-products.service';
 import Content from './carousel-content';
+import { getTranslations } from 'next-intl/server';
 
 type RelatedProductsContentProps = {
     categoryId: string,
 }
 
-export default async function RelatedProductsContent({categoryId}: RelatedProductsContentProps) {
+export default async function RelatedProductsContent({ categoryId }: RelatedProductsContentProps) {
+    // Transitions
+    const t = await getTranslations("related-products");
+
     // services
     const data = await getSimilarProductService(categoryId);
 
+    if (!data.products || data.products.length === 0) {
+        return (
+            <p className="text-center py-8">
+                {t('related-products-empty')}
+            </p>
+        );
+    }
     return (
         <Content items={data.products} />
     )
