264a834c9e799ac4f779e97615700d24bd934b8c
LaraSamara <larasamara2002@gmail.com>
2026-02-07 22:02:15 +0200

    remove token from similar products

--- src/app/[locale]/(site)/products/[productId]/_services/similar-products/similar-products.service.ts
+++ src/app/[locale]/(site)/products/[productId]/_services/similar-products/similar-products.service.ts
@@ -1,17 +1,9 @@
-import { getToken } from "@/lib/utils/manage-token";
 import { Products } from "@/lib/types/products.js";
 
 export async function getSimilarProductService(categoryId: string) {
-    const token = await getToken();
-
-    if (!token) {
-        throw new Error("No token available")
-    }
-
     const response = await fetch(`${process.env.API_URL!}/products?category=${categoryId}`, {
         method: "GET",
         headers: {
-            Authorization: `Bearer ${token.accessToken}`,
             "Content-Type": "application/json"
         },
     });
