85ca0157bd6bb294d09c9cbfdf8933e9d52b9a09
LaraSamara <larasamara2002@gmail.com>
2026-02-16 11:37:12 +0200

    Fix skeleton

--- src/components/skeletons/user-addresses/address.skeleton.tsx
+++ src/components/skeletons/user-addresses/address.skeleton.tsx
@@ -1,10 +1,12 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function AddressSkeleton() {
     return (
-        <div className="flex flex-col gap-4 rounded-md border border-zinc-300 ps-4 pe-7 pb-5 relative animate-pulse">
+        <div className="flex flex-col gap-4 rounded-md border border-zinc-300 p-5 relative">
 
             {/* Street */}
             <div className="absolute top-0 -translate-y-1/2 bg-white p-2.5">
-                <div className="h-7 w-48 bg-zinc-300 rounded-md" />
+                <Skeleton className="h-7 w-48" />
             </div>
 
             {/* Info */}
@@ -12,25 +14,24 @@ export default function AddressSkeleton() {
 
                 {/* City */}
                 <div className="flex gap-2.5 items-center">
-                    <div className="w-8 h-8 rounded-full bg-zinc-300" />
-                    <div className="h-6 w-32 bg-zinc-300 rounded-md" />
+                    <Skeleton className="w-8 h-8 rounded-full" />
+                    <Skeleton className="h-6 w-32 rounded-md" />
                 </div>
 
                 {/* Phone */}
                 <div className="flex gap-2.5 items-center">
-                    <div className="w-6 h-6 bg-zinc-300 rounded-md" />
-                    <div className="h-5 w-28 bg-zinc-300 rounded-md" />
+                    <Skeleton className="w-6 h-6 rounded-md" />
+                    <Skeleton className="h-5 w-28 rounded-md" />
                 </div>
             </div>
 
             {/* Full address */}
-            <div className="h-6 w-72 bg-zinc-200 rounded-full mt-2" />
+            <Skeleton className="h-6 w-72 rounded-full mt-2" />
 
             {/* Mutation operations */}
-            <div className="flex flex-col gap-1.5 mt-6 absolute ltr:right-0 ltr:translate-x-1/2
-                            rtl:left-0  rtl:-translate-x-1/2">
-                <div className="w-9 h-9 rounded-full border border-zinc-300 bg-zinc-200" />
-                <div className="w-9 h-9 rounded-full bg-zinc-300" />
+            <div className="flex flex-col gap-1.5 mt-6 absolute ltr:right-0 ltr:translate-x-1/2 rtl:left-0 rtl:-translate-x-1/2">
+                <Skeleton className="w-9 h-9 rounded-full border border-zinc-300" />
+                <Skeleton className="w-9 h-9 rounded-full" />
             </div>
         </div>
     );
